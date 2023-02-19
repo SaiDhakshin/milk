9bdbe4df6f4be246e16ed4172d5e95317495b459
Saidhakshin <Saidhakshin75@gmail.com>
2023-02-19 19:50:23 +0530

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <!-- Header -->
+    <Header></Header>
+    <!-- Header End-->
+    <!-- navbar -->
+    <!-- <div class="flex justify-between ...">
+      <div><img class="icon" src="./assets/logo-icon.svg" alt="" /></div>
+      <div>
+        <ul class="flex p-4">
+          <li class="mx-1.5">Home</li>
+          <li class="mx-1.5">About</li>
+          <li class="mx-1.5">FAQ</li>
+        </ul>
+      </div>
+      <div>
+        <ul class="flex p-4">
+          <li class="mx-1.5">Login</li>
+        </ul>
+      </div>
+    </div> -->
+    <!-- navbar end -->
+    <!-- showcase -->
+    <div class="banner container">
+      <img class="banner-item" src="./assets/banner2.jpg" alt="" />
+    </div>
+    <!-- showcase end -->
+    <!-- Stats -->
+    <Stats></Stats>
+    <!-- Stats end -->
+    <!-- Trusted -->
+    <Trusted></Trusted>
+  </div>
+</template>
+
+<script>
+import Stats from "./components/Stats.vue";
+import Trusted from "./components/Trusted.vue";
+import Header from "./components/Header.vue";
+export default {
+  components: {
+    Stats,
+    Trusted,
+    Header,
+  },
+};
+</script>
+
+<style></style>
